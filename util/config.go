483121329f441a96a2df217300c5b38530339c15
idoubi <me@idoubi.cc>
2026-03-01 14:44:45 +0800

    update configmap

--- util/config.go
+++ util/config.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
@@ -13,6 +14,12 @@ func InitConfig(filename string) error {
 	if err := viper.ReadInConfig(); err != nil {
 		return fmt.Errorf("read config file failed: %w", err)
 	}
+
+	// Allow environment variables to override config values.
+	// FASTCLAW_DB_PASSWORD -> db.password, FASTCLAW_API_ADMIN_TOKEN -> api.admin_token, etc.
+	viper.SetEnvPrefix("FASTCLAW")
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
 
 	viper.WatchConfig()
 	viper.OnConfigChange(func(e fsnotify.Event) {
